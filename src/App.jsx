d02949e155081f86cd543c865d0477881b2ebe25
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-07-17 17:26:21 +0300

    hw_84

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Card from "./components/Card.jsx";
+import Slider from "./components/Slider.jsx";
 
 
 class App extends React.Component {
     render() {
-        return<Card/>
+        return<Slider/>
     }
 }
 
