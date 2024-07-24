bb9e508ffd00c5909b22802dbec0dd8b6758ac09
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-07-24 19:46:43 +0300

    hw_86 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Card from "./components/Card.jsx";
+import MyForm from "./components/MyForm.jsx";
 
 
 class App extends React.Component {
     render() {
-        return<Card/>
+        return<MyForm/>
     }
 }
 
